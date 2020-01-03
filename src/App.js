670185afe2949b958c887d423612be387f3e5ea3
Goran <pavlovskigoran88@gmail.com>
2020-01-03 23:20:56 +0100

    routing wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import HomepageComponent from "./pages/homepage/homepage.component";
+import HomepageComponent from './pages/homepage/homepage.component';
+import Route from "react-router-dom/es/Route";
+import { Switch } from "react-router-dom";
+
+const HatsPage = props => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomepageComponent/>
+      <Switch>
+        <Route exact path='/' component={HomepageComponent}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
